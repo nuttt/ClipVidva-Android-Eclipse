51568fcc1e5b842d2252bfad372086625222f28e
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2013-10-01 22:56:38 +0700

    regenerate R file

--- gen/com/example/clipvidva/R.java
+++ gen/com/example/clipvidva/R.java
@@ -27,10 +27,11 @@ public final class R {
     public static final class drawable {
         public static final int askeng=0x7f020000;
         public static final int grid_item=0x7f020001;
-        public static final int hsmaths=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logo=0x7f020004;
-        public static final int splash_button=0x7f020005;
+        public static final int grid_item_correct=0x7f020002;
+        public static final int hsmaths=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int splash_button=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f090022;
