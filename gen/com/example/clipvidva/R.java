5346fb20fc5761a91dcc460326e4283f842f605c
Vee <happierv@gmail.com>
2013-10-02 12:12:20 +0700

    Actionbar Light (finally!)

--- gen/com/example/clipvidva/R.java
+++ gen/com/example/clipvidva/R.java
@@ -35,14 +35,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int askeng=0x7f020000;
-        public static final int grid_button=0x7f020001;
-        public static final int grid_item=0x7f020002;
-        public static final int grid_item_correct=0x7f020003;
-        public static final int hsmaths=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int logo=0x7f020006;
-        public static final int splash_button=0x7f020007;
-        public static final int wood_pattern=0x7f020008;
+        public static final int close=0x7f020001;
+        public static final int grid_button=0x7f020002;
+        public static final int grid_item=0x7f020003;
+        public static final int grid_item_correct=0x7f020004;
+        public static final int hsmaths=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int splash_button=0x7f020008;
+        public static final int wood_pattern=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f090024;
@@ -54,6 +55,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int choice3=0x7f09000e;
         public static final int choice4=0x7f09000f;
         public static final int choices=0x7f09000b;
+        public static final int close_activity=0x7f090025;
         public static final int fullscreen_content=0x7f090002;
         public static final int fullscreen_content_controls=0x7f090003;
         public static final int hint=0x7f090010;
@@ -108,57 +110,58 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int video_viewer=0x7f080001;
     }
     public static final class string {
-        public static final int action_setting=0x7f060024;
-        public static final int action_settings=0x7f06002a;
+        public static final int action_setting=0x7f060025;
+        public static final int action_settings=0x7f06002b;
         public static final int app_name=0x7f060000;
         public static final int askvidva_category=0x7f060003;
-        public static final int category_activity_title=0x7f06002f;
+        public static final int category_activity_title=0x7f060030;
         /**  for debug purpose 
          */
-        public static final int choice_default=0x7f060020;
+        public static final int choice_default=0x7f060021;
         public static final int choose_other_subject=0x7f06000b;
         public static final int close=0x7f06000c;
+        public static final int close_activity=0x7f060015;
         public static final int correct_desc=0x7f06000e;
         public static final int correct_title=0x7f06000d;
-        public static final int dummy_button=0x7f06002d;
-        public static final int dummy_content=0x7f06002e;
-        public static final int hello_world=0x7f06002b;
+        public static final int dummy_button=0x7f06002e;
+        public static final int dummy_content=0x7f06002f;
+        public static final int hello_world=0x7f06002c;
         public static final int hint_button=0x7f06000f;
-        public static final int hint_default=0x7f060023;
+        public static final int hint_default=0x7f060024;
         public static final int icon_correct=0x7f060014;
-        public static final int icon_pencil=0x7f060026;
-        public static final int icon_video=0x7f060025;
+        public static final int icon_pencil=0x7f060027;
+        public static final int icon_video=0x7f060026;
         public static final int incorrect_desc=0x7f060011;
         public static final int incorrect_title=0x7f060010;
         public static final int math_category=0x7f060002;
         public static final int next=0x7f06000a;
         public static final int no_hint=0x7f060013;
-        public static final int question_default=0x7f060021;
-        public static final int result_default=0x7f060022;
+        public static final int question_default=0x7f060022;
+        public static final int result_default=0x7f060023;
         /**  askeng 
          */
-        public static final int subject_askeng_ce=0x7f060015;
-        public static final int subject_askeng_che=0x7f060016;
-        public static final int subject_askeng_cp=0x7f060017;
-        public static final int subject_askeng_ee=0x7f060018;
-        public static final int subject_askeng_eng=0x7f060019;
-        public static final int subject_askeng_env=0x7f06001a;
-        public static final int subject_askeng_me=0x7f06001b;
-        public static final int subject_askeng_mn=0x7f06001c;
-        public static final int subject_askeng_mt=0x7f06001d;
-        public static final int subject_askeng_pe=0x7f06001e;
-        public static final int subject_askeng_sv=0x7f06001f;
+        public static final int subject_askeng_ce=0x7f060016;
+        public static final int subject_askeng_che=0x7f060017;
+        public static final int subject_askeng_cp=0x7f060018;
+        public static final int subject_askeng_ee=0x7f060019;
+        public static final int subject_askeng_eng=0x7f06001a;
+        public static final int subject_askeng_env=0x7f06001b;
+        public static final int subject_askeng_me=0x7f06001c;
+        public static final int subject_askeng_mn=0x7f06001d;
+        public static final int subject_askeng_mt=0x7f06001e;
+        public static final int subject_askeng_pe=0x7f06001f;
+        public static final int subject_askeng_sv=0x7f060020;
         public static final int subject_conic_section=0x7f060005;
         public static final int subject_functions=0x7f060006;
         public static final int subject_real_number=0x7f060004;
         public static final int submit_text=0x7f060009;
-        public static final int title_activity_video_full_viewer=0x7f06002c;
-        public static final int title_activity_video_viewer=0x7f060029;
+        public static final int title_activity_video_full_viewer=0x7f06002d;
+        public static final int title_activity_video_viewer=0x7f06002a;
         public static final int title_category=0x7f060008;
         public static final int title_item_detail=0x7f060001;
         public static final int title_quiz=0x7f060007;
-        public static final int title_subject_detail=0x7f060028;
-        public static final int title_subject_list=0x7f060027;
+        public static final int title_subject_detail=0x7f060029;
+        public static final int title_subject_list=0x7f060028;
         public static final int try_again=0x7f060012;
     }
     public static final class style {
@@ -167,26 +170,24 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
 
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
  API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f070002;
-        public static final int ButtonBar=0x7f070004;
-        public static final int ButtonBarButton=0x7f070005;
-        public static final int FullscreenActionBarStyle=0x7f070006;
-        public static final int FullscreenTheme=0x7f070003;
-        public static final int MyActionBar=0x7f070001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070002;
     }
     public static final class styleable {
         /** 
